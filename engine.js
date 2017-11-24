d51100495bd80e5d5c21ba44bdb71f057431a4d3
Jordan Weichel <weichel@umich.edu>
2017-11-24 17:20:19 -0600

    add engine.js

---
+++ engine.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var Engine = class Engine {
+  constructor(data){
+
+
+  };
+
+
+};
+
+var Deck = class Engine_Deck {
+  constructor(data){
+    this.stack = data.stack;
+  };
+};
+
+var Stack = class Engine_Stack {
+  constructor(data){
+
+    this.cards = data.cards;
+    //this.index = data.index;
+  };
+
+  // RANDOMIZE THE STACK OF CARDS
+  randomize(){
+
+  };
+  // ADDS A CARD TO THE STACK
+  //    card - card object to add
+  //    mode - 0 to add to top, 1 to add to bottom
+  add(card,mode){
+
+  };
+
+  // ADDS A STACK TO THE Stack
+  //    stack - stack object to add
+  //    mode - 0 to add to top, 1 to add to bottom
+  combine(stack,mode){
+
+  };
+
+};
